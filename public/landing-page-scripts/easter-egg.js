4e662d2a0778eb9248bb9f7dd5c6355ef2ad6897
nyt23 <153197020+nyt23@users.noreply.github.com>
2024-02-08 22:08:01 +0100

    Update easter-egg.js

--- public/landing-page-scripts/easter-egg.js
+++ public/landing-page-scripts/easter-egg.js
@@ -1,5 +1,5 @@
 function triggerEmoji(){
-    emojis = ["☀️", "❄️️", "🌧️", "🌩️", "☁️"]
+    emojis = ["☀️", "❄️️", "🌧️", "🌩️", "☁️", "⚡️", "🌬️", "🌥️"]
     const emoji = document.getElementById("weather-emoji")
     const index_emoji = (emojis.indexOf(emoji.textContent) + 1)%emojis.length
     emoji.textContent = emojis[index_emoji]
